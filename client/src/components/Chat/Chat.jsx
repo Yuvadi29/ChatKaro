f9626026e542e24f98d000f23a8bf4322393f23a
Yuvadi29 <adityatrivedi612@gmail.com>
2023-01-20 17:43:30 +0530

    Server Error

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
 import './Chat.css';
 
-const Chat = () => {
+let socket;
+
+const Chat = ({ location }) => {
+  
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+  const EndPoint = 'localhost:5000';
+
+  // This will return us a URL  
+  useEffect(() => {
+    const { name, room } = queryString.parse(window.location.search);
+
+    socket = io(EndPoint);
+    //To see what data we are dealing with
+    console.log(socket);  
+
+    setName(name);
+    setRoom(room);
+  },[]);
+
+
   return (
     <div>
-        <h1>Chat</h1>
+      <h1>Chat</h1>
     </div>
   )
 }
--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
+const cors = require('cors');
+
 
 const PORT = process.env.PORT || 5000;
 
@@ -23,3 +25,9 @@ const router = require('./routes/route');
 app.use(router);
 
 server.listen(PORT, () => console.log(`Server running on Port ${PORT}`));
+
+app.use(cors(
+    {
+        origin: '*',
+    }
+));
