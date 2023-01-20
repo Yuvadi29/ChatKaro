e64659fa71d07bdfc3d895b0bc54d0d34ca7ac0a
Yuvadi29 <adityatrivedi612@gmail.com>
2023-01-20 16:38:31 +0530

    Server Side Instance Creation

--- server/index.js
+++ server/index.js
@@ -2,12 +2,24 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 
+const PORT = process.env.PORT || 5000;
+
 // Setup Socket Io
 const app = express();
 const server = http.createServer(app);
 
 //Instance for Socket Io
 const io = socketio(server);
+io.on('connection', (socket) => {
+    console.log('We have a new Connection');
 
-const PORT = process.env.PORT || 5000;
+    socket.on('disconnect', () => {
+        console.log('User Disconnected');
+    });
+});
+
+
+const router = require('./routes/route');
+app.use(router);
+
 server.listen(PORT, () => console.log(`Server running on Port ${PORT}`));
